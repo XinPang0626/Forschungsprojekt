fec87440f759cbe5151e9766c1239f4a97f68ed3
flaXme <44303115+flaXme@users.noreply.github.com>
2020-12-10 16:36:30 +0100

    modified version of simple Route-planer
    -data structure modified to allow using double-valued distance 
    -modified Dijkstra adopts personalised  cost parameter alpha
    -need to proof of correctness

---
+++ Forschungsprojekt/src/Routerplaner/Dijkstra.java
@@ -0,0 +1,69 @@
+package Routerplaner;
+
+import java.util.Arrays;
+
+public class Dijkstra {
+	private double[] dis;
+	private int[] parent;
+	private double[] alpha;
+	
+	
+	Dijkstra(Graph graph, int start, double[] alpha){
+		System.out.println("computing dijkstra...");
+		long sTime = System.currentTimeMillis();
+		this.dis = new double[graph.getNodeNr()];
+		this.parent = new int[graph.getNodeNr()];
+		this.alpha = alpha;
+		
+		for (int i = 0; i < parent.length; i++) {
+			dis[i] = Double.MAX_VALUE;
+			parent[i] = -1; // no parent
+		}
+		
+		parent[start] = start;
+		dis[start] = 0;
+		
+		MinHeap heap = new MinHeap(graph.getNodeNr());
+		
+		heap.add(start, 0);
+		
+		while(heap.getSize() > 0) {
+			double[] min = heap.remove();
+			double[] out = graph.getOutgingEdgesArray((int)min[0]);
+			
+			if(out != null) {
+				for (int i = 0; i < out.length; i += (1+alpha.length)) {
+					double[] costVector =  Arrays.copyOfRange(out, i+1, i+alpha.length + 1);
+					if (dis[(int)min[0]] + dotProduct(alpha, costVector) < dis[(int)out[i]]) {
+						dis[(int)out[i]] = dis[(int)min[0]] + dotProduct(alpha, costVector);
+						parent[(int)out[i]] = (int)min[0];
+						if (heap.posInHeap[(int)out[i]] != -1) {// in heap
+							heap.decreaseKey((int)out[i], dis[(int)out[i]]);
+						}else {
+							heap.add((int)out[i], dis[(int)out[i]]);
+						}
+					}
+				}
+				
+				
+			}
+			
+		}
+		long eTime = System.currentTimeMillis();
+		long time = eTime - sTime;
+		System.out.println("Dijkstra Computation took ["+time+"] milli seconds");
+		
+	}
+	
+	int getShortestPathTo(int nodeID) {
+		return this.parent[nodeID];
+	}
+	double dotProduct(double a[], double b[]) {
+		double sum = 0;
+		for(int i = 0; i < a.length; i++) {
+			sum += a[i] * b[i];
+		}
+		return sum;
+	}
+
+}
---
+++ Forschungsprojekt/src/Routerplaner/Main.java
@@ -0,0 +1,110 @@
+package Routerplaner;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.io.FileWriter;
+
+public class Main {
+	static Graph graph;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter path to File: ");
+		String path = scanner.nextLine();
+		graph = new Graph(path);
+		System.out.println("Enter the personal cost coefficient alpha(2 positive number seperate with one space): ");
+		String parameter = scanner.nextLine();
+		String[] tempStringArray = parameter.split(" ");
+		double[] alpha = new double[2];
+		alpha[0] = Double.parseDouble(tempStringArray[0]);
+		alpha[1] = Double.parseDouble(tempStringArray[1]);
+		while (true) {
+			System.out.println("choose function:");
+			System.out.println("---------------------------------------");
+			System.out.println("| 1: Dijkstra with start-target query |");
+			System.out.println("|-------------------------------------|");
+			System.out.println("| 2: Dijkstra with start and query    |");
+			System.out.println("|-------------------------------------|");
+			System.out.println("| 3: File                             |");
+			System.out.println("|-------------------------------------|");
+			System.out.println("| 4: End                              |");
+			System.out.println("---------------------------------------");
+			String function = scanner.next();
+
+			if (function.equals("1") || function.equals("2") || function.equals("3")) {
+				if (function.equals("1")) 
+				{
+					System.out.println("source: ");
+					int source = scanner.nextInt();
+					System.out.println("target: ");
+					int target = scanner.nextInt();
+					Dijkstra dijk = new Dijkstra (graph, source, alpha);
+					System.out.println("shortest path between "+ source +" and "+ target + ": "+ dijk.getShortestPathTo(target));
+					
+				} 
+				else if (function.equals("2")) 
+				{
+					System.out.println("start:	");
+					int start = scanner.nextInt();
+					Dijkstra dijk = new Dijkstra (graph, start, alpha);
+					while(true) {
+						System.out.println("Do you want to query? y/n  ");
+						String answer = scanner.next();
+						if (answer.equals("n")) {
+							break;
+						}else if (answer.equals("y")) {
+							System.out.println("target: ");
+							int target = scanner.nextInt();
+							System.out.println("shortest path between "+ start +" and "+ target + ": "+ dijk.getShortestPathTo(target));
+						}
+					}
+					
+				} 
+				else if (function.equals("3")) 
+				{	
+					System.out.println("Enter file path: ");
+					String path2 = scanner.next();
+					BufferedReader br = null;
+					FileWriter fileWriter = null;
+					PrintWriter printWriter = null;
+					String line = null;
+					String[] split;
+					int acutalStart = -1;
+					Dijkstra dijk = null;
+					try {
+						br = new BufferedReader(new FileReader(path2));
+						fileWriter = new FileWriter("result.txt");
+						printWriter = new PrintWriter(fileWriter);
+						while((line = br.readLine()) != null) {
+							split = line.split(" ");
+							int start = Integer.parseInt(split[0]);
+							int target = Integer.parseInt(split[1]);
+							if (acutalStart != start) {
+								dijk = new Dijkstra (graph, start, alpha);
+							}
+							printWriter.println(dijk.getShortestPathTo(target));
+							System.out.println(dijk.getShortestPathTo(target));
+							acutalStart = start;
+						}
+						printWriter.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			} else if (function.equals("4")) {
+				System.out.println("successfully ended");
+				scanner.close();
+				break;
+			}
+		}
+
+	}
+
+}
