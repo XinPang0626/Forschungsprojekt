a09f948a8b98c5f301427ad966ae7b716cf621f7
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-01-02 20:46:26 +0100

    leaflet in angular

--- angularclient/src/app/map/map.component.ts
+++ angularclient/src/app/map/map.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
+  private map;
+  defaultla: number = 48.6674;
+  defaultlng: number = 9.2445;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.initMap();
   }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [this.defaultla, this.defaultlng],
+      zoom: 20
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+
 
 }
