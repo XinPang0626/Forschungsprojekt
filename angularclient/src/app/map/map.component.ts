56c005ad70a17bc62b39bf889cc1af6891e152d3
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-01-26 00:30:41 +0100

    -

--- angularclient/src/app/map/map.component.ts
+++ angularclient/src/app/map/map.component.ts
@@ -50,7 +50,7 @@ export class MapComponent implements AfterViewInit {
   }
   //string be parsed into nodearray
   parseNodeString(nodeString: string): number[][] {
-    nodeString=nodeString.substring(1, nodeString.length-1);
+    nodeString = nodeString.substring(1, nodeString.length - 1);
     var nodeArray: number[][] = new Array();
     var split = nodeString.split(",");
     for (let i = 0; i < split.length; i += 2) {
@@ -70,7 +70,7 @@ export class MapComponent implements AfterViewInit {
         this.makeaLINE(array);
         console.log("dijpath loaded");
       })
-    }else {
+    } else {
       alert('load a path first')
     }
 
@@ -130,9 +130,42 @@ export class MapComponent implements AfterViewInit {
     }).addTo(this.map);
   }
 
+  makeeachDot() {
+    var array: number[][];
+    for (let i = 0; i < array.length, i++;) {
+      var long = array[i][0];
+      var lat = array[i][1];
+
+      var mydot = {
+        "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {"value": "Foo"},
+            "geometry": {
+              "type": "Point",
+              "coordinates": [long, lat]
+            }
+          }
+        ]
+      }
+      L.geoJSON(mydot).addTo(this.map);
+    }
+
+  }
+
+
 
   changeMapp(la: number, lng: number) {
     this.map.panTo(new L.LatLng(la, lng));
+  }
+
+  onEachFeature(feature, layer): void {
+    layer.on('click', function (e) {
+      //alert(feature.properties.popupContent);
+      //or
+      alert(feature.properties.id);
+    });
   }
 
   sendpath(input: string) {
