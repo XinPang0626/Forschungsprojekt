9f7bc6e2ffbbc72da14f8ddce998c9ba9b30ca1c
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-02-01 22:27:12 +0100

    format

--- spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
+++ spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
@@ -8,8 +8,6 @@ import com.forschungsprojekt.spring_backend.localization.Quadtree;
 import com.forschungsprojekt.spring_backend.routerplaner.AStar_Standard;
 import com.forschungsprojekt.spring_backend.routerplaner.Dijkstra;
 import com.forschungsprojekt.spring_backend.routerplaner.Graph;
-
-import org.json.JSONObject;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MapController {
     String decodedpath;
-    // dummy string format be: (long, lat),(long, lat),(long,lat).... due Geojson reading in that format
+    // dummy string format be: [[long, lat),(long, lat),(long,lat).... due Geojson reading in that format
     String cordinates = "[[-104.98809814453125, 39.76632525654491],[-104.9359130859375,39.751017451967144],[-104.974365234375, 39.720919782725545]]";
     
     // here to only revoke to get the nodes back at first
@@ -30,8 +28,7 @@ public class MapController {
         decodedpath = URLDecoder.decode(path, StandardCharsets.UTF_8);
         System.out.println(decodedpath);
         Graph graph= new Graph(decodedpath);
-
-        return cordinates;
+        return "[["+graph.getLongitude(0)+","+graph.getLatitude(0)+"]]";
     } // here to revoke algorithm and turn the result into string to be returned
 
     @CrossOrigin(origins = "http://localhost:4200")
@@ -83,13 +80,13 @@ public class MapController {
         Quadtree quadtree = new Quadtree(decodedpath);
         String[] alphaStringArray = alpha.split(" ");
         double[] doubleAlpha = Arrays.stream(alphaStringArray).mapToDouble(Double::parseDouble).toArray();
-        String[] startLatLon = start.split(",");
+        String[] startLatLon = start.split(" ");
         double[] doubleLatLon = Arrays.stream(startLatLon).mapToDouble(Double::parseDouble).toArray();
         double startLat = doubleLatLon[0];
         double startLon = doubleLatLon[1];
         int startPoint = quadtree.nextNeighbor(startLat, startLon);
 
-        String[] endLatLon = start.split(",");
+        String[] endLatLon = end.split(" ");
         doubleLatLon = Arrays.stream(endLatLon).mapToDouble(Double::parseDouble).toArray();
         double endLat = doubleLatLon[0];
         double endLon = doubleLatLon[1];
@@ -114,13 +111,13 @@ public class MapController {
          Quadtree quadtree = new Quadtree(decodedpath);
          String[] alphaStringArray = alpha.split(" ");
          double[] doubleAlpha = Arrays.stream(alphaStringArray).mapToDouble(Double::parseDouble).toArray();
-         String[] startLatLon = start.split(",");
+         String[] startLatLon = start.split(" ");
          double[] doubleLatLon = Arrays.stream(startLatLon).mapToDouble(Double::parseDouble).toArray();
          double startLat = doubleLatLon[0];
          double startLon = doubleLatLon[1];
          int startPoint = quadtree.nextNeighbor(startLat, startLon);
 
-         String[] endLatLon = start.split(",");
+         String[] endLatLon = end.split(" ");
          doubleLatLon = Arrays.stream(endLatLon).mapToDouble(Double::parseDouble).toArray();
          double endLat = doubleLatLon[0];
          double endLon = doubleLatLon[1];
