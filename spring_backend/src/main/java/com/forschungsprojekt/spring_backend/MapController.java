4d1108b18c6acf6a637c4687a77b648f36d482df
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-02-20 19:26:17 +0100

    enter path in controller

--- spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
+++ spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
@@ -3,11 +3,14 @@ package com.forschungsprojekt.spring_backend;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Scanner;
 
 import com.forschungsprojekt.spring_backend.localization.Quadtree;
 import com.forschungsprojekt.spring_backend.routerplaner.AStar_Standard;
 import com.forschungsprojekt.spring_backend.routerplaner.Dijkstra;
 import com.forschungsprojekt.spring_backend.routerplaner.Graph;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MapController {
+    static Graph testgraph;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter path to File: ");
+        String path = scanner.nextLine();
+        testgraph = new Graph(path);
+        scanner.close();
+		SpringApplication.run(SpringBackendApplication.class, args);
+	}
     String decodedpath;
     // dummy string format be: [[long, lat),(long, lat),(long,lat).... due Geojson reading in that format
     String cordinates = "[[-104.98809814453125, 39.76632525654491],[-104.9359130859375,39.751017451967144],[-104.974365234375, 39.720919782725545]]";
@@ -27,8 +40,8 @@ public class MapController {
     public String sendCordinates(@RequestParam(name = "path") String path) {
         decodedpath = URLDecoder.decode(path, StandardCharsets.UTF_8);
         System.out.println(decodedpath);
-        Graph graph= new Graph(decodedpath);
-        return "[["+graph.getLongitude(0)+","+graph.getLatitude(0)+"]]";
+        //Graph graph= new Graph(decodedpath);
+        return "[["+testgraph.getLongitude(0)+","+testgraph.getLatitude(0)+"]]";
     } // here to revoke algorithm and turn the result into string to be returned
 
     @CrossOrigin(origins = "http://localhost:4200")
@@ -53,14 +66,14 @@ public class MapController {
     @ResponseBody
     public String sendAstar(@RequestParam(name = "path") String path, @RequestParam(name = "start") int start,
             @RequestParam(name = "end") int end, @RequestParam(name = "alpha") String alpha, @RequestParam(name = "type") String type,
-            @RequestParam(name = "landmark") int landmark) {
+            @RequestParam(name = "landmark") int landmark, @RequestParam(name = "candidate") int candidate) {
                 //type is now Standard and ALT
         decodedpath = URLDecoder.decode(path, StandardCharsets.UTF_8);
         System.out.println(decodedpath);
         Graph graph = new Graph(decodedpath);
         String[] alphaStringArray = alpha.split(" ");
         double[] doubleAlpha = Arrays.stream(alphaStringArray).mapToDouble(Double::parseDouble).toArray();
-        AStar_Standard aStar = new AStar_Standard(graph, start, end, doubleAlpha, type, landmark);
+       AStar_Standard aStar = new AStar_Standard(graph, start, end, type, landmark, candidate);
         cordinates = aStar.getShortestPathInLonLat(end);
         return cordinates;
     }
@@ -72,7 +85,7 @@ public class MapController {
     @ResponseBody
     public String sendAstarcor(@RequestParam(name = "path") String path, @RequestParam(name = "start") String start,
             @RequestParam(name = "end") String end, @RequestParam(name = "alpha") String alpha, @RequestParam(name = "type") String type,
-            @RequestParam(name = "landmark") int landmark) {
+            @RequestParam(name = "landmark") int landmark, @RequestParam(name = "candidate") int candidate) {
                 //type is now Standard and ALT
         decodedpath = URLDecoder.decode(path, StandardCharsets.UTF_8);//path decoding
         System.out.println(decodedpath);
@@ -91,7 +104,7 @@ public class MapController {
         double endLon = doubleEndLatLon[1];//get the longtitud of end point in double
         int endPoint = quadtree.nextNeighborWithReset(endLat, endLon);//compute the coorespind point in datastructure/quadtree
 
-        AStar_Standard aStar = new AStar_Standard(quadtree.getGraph(), startPoint, endPoint, doubleAlpha, type, landmark);
+        AStar_Standard aStar = new AStar_Standard(quadtree.getGraph(), startPoint, endPoint,type, landmark, candidate);
         cordinates = aStar.getShortestPathInLonLat(endPoint);
 
         return cordinates;
