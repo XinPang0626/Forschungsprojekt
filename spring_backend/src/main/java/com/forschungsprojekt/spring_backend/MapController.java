04a494c53171325da8b959358822563f0ddd8f43
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-04-13 15:41:28 +0200

    adding files into backend directory

--- spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
+++ spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
@@ -11,6 +11,8 @@ import com.forschungsprojekt.spring_backend.localization.Quadtree;
 import com.forschungsprojekt.spring_backend.routerplaner.AStar_Standard;
 import com.forschungsprojekt.spring_backend.routerplaner.Dijkstra;
 import com.forschungsprojekt.spring_backend.routerplaner.Graph;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MapController {
     // declare variables that should be accessed by every method in this
-    Graph graph;
+    static Graph graph;
     Quadtree quadtree;
     AStar_Standard aStar;
     String decodedpath;
@@ -29,6 +31,12 @@ public class MapController {
     // dummy string format be: [[long, lat),(long, lat),(long,lat).... due Geojson
     // reading in that format
     String cordinates = "[[-104.98809814453125, 39.76632525654491],[-104.9359130859375,39.751017451967144],[-104.974365234375, 39.720919782725545]]";
+    public static void main(String[] args) {
+		SpringApplication.run(SpringBackendApplication.class, args);
+        graph= new Graph("./spring_backend/src/main/resources/bremen.graph");
+        // spring_backend\src\main\resources\bremen.graph
+        //./spring_backend/src/main/resources/bremen.graph
+	}
 
     /**
      * @param path builds graph and quadtree once, so it doesn't need to be builld
@@ -40,7 +48,7 @@ public class MapController {
     public String sendCordinates(@RequestParam(name = "path") String path) {
         decodedpath = URLDecoder.decode(path, StandardCharsets.UTF_8);
         System.out.println(decodedpath);
-        graph = new Graph(decodedpath);
+        //graph = new Graph(decodedpath);
         quadtree = new Quadtree(decodedpath);
         return "[[" + graph.getLongitude(0) + "," + graph.getLatitude(0) + "]]";
     }
