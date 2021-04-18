667457de1fc4c82a5093bfbb2300ae71916428b3
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-04-18 17:23:32 +0200

    automatic backend start+ configreader

--- spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
+++ spring_backend/src/main/java/com/forschungsprojekt/spring_backend/MapController.java
@@ -1,8 +1,6 @@
 package com.forschungsprojekt.spring_backend;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -23,8 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MapController {
     // declare variables that should be accessed by every method in this
     static Graph graph;
-    Quadtree quadtree;
-    AStar_Standard aStar;
+    static Quadtree quadtree;
+    static int landmarks;
+    static AStar_Standard aStar_Standard;
+    static AStar_Standard aStar_Alt;
     String decodedpath;
     int endPoint;
     int startPoint;
@@ -32,24 +32,26 @@ public class MapController {
     // reading in that format
     String cordinates = "[[-104.98809814453125, 39.76632525654491],[-104.9359130859375,39.751017451967144],[-104.974365234375, 39.720919782725545]]";
     public static void main(String[] args) {
+
 		SpringApplication.run(SpringBackendApplication.class, args);
-        graph= new Graph("./spring_backend/src/main/resources/bremen.graph");
-        // spring_backend\src\main\resources\bremen.graph
-        //./spring_backend/src/main/resources/bremen.graph
+        ReadConfig.read("./spring_backend/src/main/resources/config.txt");
+       graph= new Graph(ReadConfig.filepath);
+       quadtree= new Quadtree(ReadConfig.filepath);
+       landmarks= ReadConfig.nrLandmarks;
+
+       aStar_Standard= new AStar_Standard(graph, "Standard", ReadConfig.nrLandmarks);
+       aStar_Alt= new AStar_Standard(graph, "ALT", ReadConfig.nrLandmarks);
+      
+      
 	}
 
     /**
-     * @param path builds graph and quadtree once, so it doesn't need to be builld
      * @return moves to the center of the new map in frontend
      */
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping(value = "/api")
     @ResponseBody
-    public String sendCordinates(@RequestParam(name = "path") String path) {
-        decodedpath = URLDecoder.decode(path, StandardCharsets.UTF_8);
-        System.out.println(decodedpath);
-        //graph = new Graph(decodedpath);
-        quadtree = new Quadtree(decodedpath);
+    public String sendCordinates() {
         return "[[" + graph.getLongitude(0) + "," + graph.getLatitude(0) + "]]";
     }
 
@@ -98,28 +100,6 @@ public class MapController {
         return cordinates;
     }
 
-    /**
-     * load the first pre-calculation of A* 
-     * 
-     */
-    @CrossOrigin(origins = "http://localhost:4200")
-    @RequestMapping("/astarload")
-    @ResponseBody
-    public String sendAstar(  @RequestParam(name = "type") String type) {
-        // type is now Standard or ALT
-        int landmark=2;
-        try {
-			BufferedReader br = new BufferedReader(new FileReader("./spring_backend/src/main/resources/NrLandmarks.txt"));
-			 String line = br.readLine();
-             landmark= Integer.parseInt(line);
-            System.out.println(landmark); 
-            aStar = new AStar_Standard(graph, type, landmark);
-            br.close();
-        }catch (IOException e) {
-			e.printStackTrace();
-		}
-        return "loaded";
-    }
 
     /**
      * aktualisiere mit neuen Node start und end feld nur nachdem man sendAstar
@@ -128,14 +108,27 @@ public class MapController {
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping("/astarpath")
     @ResponseBody
-    public String resendAstar(@RequestParam(name = "start") int start, @RequestParam(name = "end") int end,  @RequestParam(name = "alpha") String alpha) {
+    public String resendAstar(@RequestParam(name = "start") int start, @RequestParam(name = "end") int end,  @RequestParam(name = "alpha") String alpha,
+     @RequestParam(name = "type") String type) {
         String[] alphaStringArray = alpha.split(" ");
         double[] doubleAlpha = Arrays.stream(alphaStringArray).mapToDouble(Double::parseDouble).toArray();
-        aStar.setStart(start);
-        aStar.setTarget(end);
-        aStar.setAlpha(doubleAlpha);
-        aStar.compute();
-         cordinates = aStar.getShortestPathInLonLat(end);
+        if(type.equals("Standard")){
+            aStar_Standard.setStart(start);
+            aStar_Standard.setTarget(end);
+            aStar_Standard.setAlpha(doubleAlpha);
+            aStar_Standard.compute();
+            cordinates = aStar_Standard.getShortestPathInLonLat(end);
+
+        }else if(type.equals("ALT")){
+            aStar_Alt.setStart(start);
+            aStar_Alt.setTarget(end);
+            aStar_Alt.setAlpha(doubleAlpha);
+            aStar_Alt.compute();
+            cordinates = aStar_Alt.getShortestPathInLonLat(end);
+
+        }
+      
+       
         System.out.println("A* " + " start with " + start + "-" + end);
 
         return cordinates;
@@ -154,17 +147,28 @@ public class MapController {
     @CrossOrigin(origins = "http://localhost:4200")
     @RequestMapping("/astarcor")
     @ResponseBody
-    public String sendAstarcor(@RequestParam(name = "start") String start, @RequestParam(name = "end") String end, @RequestParam(name = "alpha") String alpha) {
+    public String sendAstarcor(@RequestParam(name = "start") String start, @RequestParam(name = "end") String end, @RequestParam(name = "alpha") String alpha,
+     @RequestParam(name = "type") String type) {
         // type is now Standard and ALT
         System.out.println("computing A* from "+startPoint+ "-"+endPoint);
         String[] alphaStringArray = alpha.split(" ");//split the value of cost vector in String
         double[] doubleAlpha = Arrays.stream(alphaStringArray).mapToDouble(Double::parseDouble).toArray();//convert String value to double
-        
-        aStar.setStart(startPoint);
-        aStar.setTarget(endPoint);
-        aStar.setAlpha(doubleAlpha);
-        aStar.compute();
-        cordinates = aStar.getShortestPathInLonLat(endPoint); 
+
+        if(type.equals("Standard")){
+            aStar_Standard.setStart(startPoint);
+            aStar_Standard.setTarget(endPoint);
+            aStar_Standard.setAlpha(doubleAlpha);
+            aStar_Standard.compute();
+            cordinates = aStar_Standard.getShortestPathInLonLat(endPoint);
+
+        }else if(type.equals("ALT")){
+            aStar_Alt.setStart(startPoint);
+            aStar_Alt.setTarget(endPoint);
+            aStar_Alt.setAlpha(doubleAlpha);
+            aStar_Alt.compute();
+            cordinates = aStar_Alt.getShortestPathInLonLat(endPoint);
+
+        }
         
         
 
