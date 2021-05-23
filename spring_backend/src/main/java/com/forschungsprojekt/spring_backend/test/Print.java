ebd5706c5be3e00fe5fe7899b466226f70e385c2
MahRukhChaudhry <75682696+MahRukhChaudhry@users.noreply.github.com>
2021-05-23 14:30:08 +0200

    checking if  server only runs this file

---
+++ spring_backend/src/main/java/com/forschungsprojekt/spring_backend/test/Print.java
@@ -0,0 +1,8 @@
+package com.forschungsprojekt.spring_backend.test;
+
+public class Print {
+    
+    public static void main(String[] args) {
+      System.out.println("this is only executed");
+    }
+}
